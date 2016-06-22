d90ee7a0b31995354f71d3df5c169203c2ee8554
Mihail Bodrov <connormiha1@gmail.com>
2016-06-23 00:42:28 +0300

    update tour

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>My First {{foo}} Angular 2 App</h1>'
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div>
+            <label>name: </label>
+            <input [(ngModel)]="hero.name" placeholder="name">
+        </div>
+    `
 })
 export class AppComponent {
-    foo = "bar"
+    title: string = 'Tour of Heroes';
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
+
+export class Hero {
+    id: number;
+    name: string;
 }

7389dd1883ef9062dfcfd6a006e80639b01348d5
Mihail Bodrov <connormiha1@gmail.com>
2016-06-26 14:06:43 +0300

    3th step

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,18 +1,16 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Hero} from './hero';
+import {HeroDetailComponent} from './hero-detail.component';
+import {HeroService} from './hero.service';
 import './style.styl';
 
 @Component({
     selector: 'my-app',
+    directives: [HeroDetailComponent],
+    providers: [HeroService],
     template: `
         <h1>{{title}}</h1>
-        <div *ngIf="selectedHero">
-            <h2>{{selectedHero.name}} details!</h2>
-            <div><label>id: </label>{{selectedHero.id}}</div>
-            <div>
-                <label>name: </label>
-                <input [(ngModel)]="selectedHero.name" placeholder="name">
-            </div>
-        </div>
+        <my-hero-detail [hero]="selectedHero"></my-hero-detail>
         <h2>My Heroes</h2>
         <ul class="heroes">
             <li *ngFor="let hero of heroes"
@@ -24,29 +22,24 @@ import './style.styl';
         </ul>
     `
 })
-export class AppComponent {
-    title: string = 'Tour of Heroes';
+export class AppComponent implements OnInit {
+    title = 'Tour of Heroes';
+    heroes: Hero[];
     selectedHero: Hero;
-    public heroes = HEROES;
+
+    constructor(private heroService: HeroService) {
+
+    }
+
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
     }
 }
-
-export class Hero {
-    id: number;
-    name: string;
-}
-
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
 import 'polyfill';
-import { bootstrap }  from '@angular/platform-browser-dynamic';
-import { AppComponent } from './app.component';
+import {bootstrap}  from '@angular/platform-browser-dynamic';
+import {AppComponent} from './app.component';
 bootstrap(AppComponent);
